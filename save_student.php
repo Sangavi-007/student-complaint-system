7c1f9cc384e80e13697ed840055f92bd7380c313
Sangavi-007 <thamotharansangavi@gmail.com>
2026-04-25 19:10:01 +0530

    made changes

---
+++ save_student.php
@@ -0,0 +1,24 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "complaint_system");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$regno = $_POST['regno'];
+$email = $_POST['email'];
+$dept = $_POST['dept'];
+$contact = $_POST['contact'];
+
+$sql = "INSERT INTO students (name, regno, email, dept, contact)
+VALUES ('$name','$regno','$email','$dept','$contact')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Student details saved successfully!";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
